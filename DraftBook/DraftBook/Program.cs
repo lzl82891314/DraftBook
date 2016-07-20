c3ee85afbf2a6f19f68b5814a25644336abf7ded
lzl82891314 <lzl82891314@163.com>
2016-07-20 08:23:03 +0800

    加密方法

---
+++ DraftBook/DraftBook/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DraftBook.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region 加密方法测试
+            EncryptTest();
+            #endregion
+        }
+
+        static void EncryptTest()
+        {
+            var encryptedStr = EncryptTool.EncryptHelper.DESEncrypt("加密方法测试", "Home.Eop", "Home.Eop").Result;
+            System.Console.WriteLine(encryptedStr ?? "加密失败");
+            if (encryptedStr != null)
+            {
+                var decryptedStr = EncryptTool.EncryptHelper.DESDecrypt(encryptedStr, "Home.Eop", "Home.Eop").Result;
+                System.Console.WriteLine(decryptedStr);
+            }
+
+            System.Console.ReadKey();
+        }
+    }
+}
---
+++ DraftBook/EncryptTool/EncryptHelper.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DraftBook.EncryptTool
+{
+    public class EncryptHelper
+    {
+        public static async Task<string> DESEncrypt(string sourceStr, string key, string iv, string charset = "utf-8")
+        {
+            try
+            {
+                var encodingObj = Encoding.GetEncoding(charset);
+                var keyArr = encodingObj.GetBytes(key);
+                var ivArr = encodingObj.GetBytes(iv);
+
+                DESCryptoServiceProvider desObj = new DESCryptoServiceProvider();
+                using (var msObj = new MemoryStream())
+                {
+                    var sourceArr = encodingObj.GetBytes(sourceStr);
+                    try
+                    {
+                        using (var csObj = new CryptoStream(msObj, desObj.CreateEncryptor(keyArr, ivArr), CryptoStreamMode.Write))
+                        {
+                            await csObj.WriteAsync(sourceArr, 0, sourceArr.Length);
+                            await csObj.FlushAsync();
+                        }
+
+                        return Convert.ToBase64String(msObj.ToArray());
+                    }
+                    catch (Exception innerEx)
+                    {
+                        //LogFunc(innerEx.Message);
+                        return null;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                //LogFunc(ex.Message);
+                return null;
+            }
+        }
+
+        public static async Task<string> DESDecrypt(string encryptedStr, string key, string iv, string charset = "utf-8")
+        {
+            try
+            {
+                var encodingObj = Encoding.GetEncoding(charset);
+                var keyArr = encodingObj.GetBytes(key);
+                var ivArr = encodingObj.GetBytes(iv);
+
+                DESCryptoServiceProvider desObj = new DESCryptoServiceProvider();
+                using (var msObj = new MemoryStream())
+                {
+                    var encryptedArr = Convert.FromBase64String(encryptedStr);
+                    try
+                    {
+                        using (var csObj = new CryptoStream(msObj, desObj.CreateDecryptor(keyArr, ivArr), CryptoStreamMode.Write))
+                        {
+                            await csObj.WriteAsync(encryptedArr, 0, encryptedArr.Length);
+                            await csObj.FlushAsync();
+                        }
+                        return encodingObj.GetString(msObj.ToArray());
+                    }
+                    catch (Exception innerEx)
+                    {
+                        //LogFunc(innerEx.Message);
+                        return null;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                //LogFunc(ex.Message);
+                return null;
+            }
+        }
+    }
+}
